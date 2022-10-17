071e850ef8199838805500c64fd46e91221bf003
Hardik Arora <76209465+hardikkk20@users.noreply.github.com>
2022-10-17 17:04:03 +0530

    v1.1

---
+++ src/com/pixel/Contrast.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.pixel;
+
+import java.awt.image.BufferedImage;
+
+/**
+ *
+ * @author HP
+ */
+public class Contrast {
+
+    public Contrast(BufferedImage image) {
+    }
+
+    
+}
